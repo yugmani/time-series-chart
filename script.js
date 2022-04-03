2000c0278b7fad20e232a5d5fb23c54ec5f6499d
yugmani <yugmani@yahoo.com>
2022-04-03 16:50:21 -0700

    Initial commit

---
+++ script.js
@@ -0,0 +1,26 @@
+const ctx = document.getElementById('timeSeriesChart').getContext('2d');
+
+const startDate = new Date(2020, 0, 1);
+
+const labels = [];
+for (let i = 0; i < 6; i++) {
+  const date = moment(startDate).add(i, 'days').format('YYYY-MM-DD');
+  labels.push(date.toString());
+}
+
+const chart = new Chart(ctx, {
+  type: 'line',
+  data: {
+    labels,
+    datasets: [
+      {
+        label: '#of Votes',
+        data: [12, 19, 3, 5, 2, 3],
+        borderWidth: 1,
+        fill: false,
+        borderColor: 'blue',
+      },
+    ],
+  },
+  options: {},
+});
